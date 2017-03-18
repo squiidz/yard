1f1e825b9d1796aa39921a4eae740cde81c06d98
jchaput <piinkyz05@gmail.com>
2017-03-18 18:35:50 -0400

    fixed bench

--- benches/shunting_yard_bench.rs
+++ benches/shunting_yard_bench.rs
@@ -2,12 +2,10 @@
 extern crate test;
 extern crate yard;
 
-use yard::{eval, parse};
-
 #[bench]
 fn bench_shunting_yard(b: &mut test::Bencher) {
     let code = "4 + 3 * (1 + 4)";
     b.iter(|| {
-        eval(parse(code).expect("Error generating tokens"));
+        yard::evaluate(code);
     });
 }
