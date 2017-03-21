46e41705d23e0745ff3a891ccb1d2c29be6a2203
jchaput <piinkyz05@gmail.com>
2017-03-21 14:14:40 -0400

    added bench tests

--- benches/shunting_yard_bench.rs
+++ benches/shunting_yard_bench.rs
@@ -3,9 +3,51 @@ extern crate test;
 extern crate yard;
 
 #[bench]
-fn bench_shunting_yard(b: &mut test::Bencher) {
+fn bench_simple_operation(b: &mut test::Bencher) {
+    let code = "4 + 3";
+    b.iter(|| {
+        if let Ok(t) = yard::evaluate(code) {
+            assert_eq!(t, 7);
+        }
+    });
+}
+
+#[bench]
+fn bench_complex_operation(b: &mut test::Bencher) {
     let code = "4 + 3 * (1 + 4) + 2 ^ 3";
     b.iter(|| {
-        let _ = yard::evaluate(code);
+        if let Ok(t) = yard::evaluate(code) {
+            assert_eq!(t, 27);
+        }
+    });
+}
+
+#[bench]
+fn bench_big_operation(b: &mut test::Bencher) {
+    let code = "3000 + 72000";
+    b.iter(|| {
+        if let Ok(t) = yard::evaluate(code) {
+            assert_eq!(t, 75000);
+        }
+    });
+}
+
+#[bench]
+fn bench_big_complex_operation(b: &mut test::Bencher) {
+    let code = "412 + 34 * (213 + 40 ^ 2) + 122 ^ 3";
+    b.iter(|| {
+        if let Ok(t) = yard::evaluate(code) {
+            assert_eq!(t, 1877902);
+        }
+    });
+}
+
+#[bench]
+fn bench_nested_operation(b: &mut test::Bencher) {
+    let code = "4 * (3 + (7 * (3 - 1) / 2)) + 8)";
+    b.iter(|| {
+        if let Ok(t) = yard::evaluate(code) {
+            assert_eq!(t, 48);
+        }
     });
 }
