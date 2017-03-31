3155ab7e7de466093b7fa75d8e9db8a2483474b5
jchaput <piinkyz05@gmail.com>
2017-03-31 11:45:39 -0400

    added basic repl

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,38 @@
 extern crate yard;
 use std::env;
+use std::io;
+use std::io::prelude::*;
 
 fn main() {
     let inp = env::args().collect::<Vec<String>>();
     if inp.len() >= 2 {
-        let code = inp.last().unwrap();
-        match yard::evaluate(code) {
+        match yard::evaluate(inp.last().unwrap()) {
             Ok(v) => println!("{}", v),
             Err(e) => println!("{}", e),
         }
     } else {
-        println!("Equation arguments required.");
+        repl();
+        return
+    };
+}
+
+fn repl() {
+    let inp = io::stdin();
+    let mut inp = inp.lock();
+
+    loop {
+        print!(">>> ");
+        io::stdout().flush().unwrap();
+        let mut buff = String::new();
+        match inp.read_line(&mut buff) {
+            Ok(_) => {
+                match yard::evaluate(&buff) {
+                    Ok(v) => println!("{}", v),
+                    Err(e) => println!("{}", e),
+                }
+            },
+            Err(e) => println!("{}", e),
+        };
+        buff.clear();
     }
 }
